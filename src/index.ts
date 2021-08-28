df849bdc8c13ec05e5e0d1abae2686e184225d51
ElzaanVan <elzaanvanl@gmail.com>
2021-08-28 13:06:39 +0200

    Added Model Properties

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import { UserForm } from "./views/UserForm";
+import { User } from './models/User';
 
 // Rendering Userform in HTML
-const userForm = new UserForm(document.getElementById('root'));
+const user = User.buildUser({ name: "Denti",  age: 8 });
+
+const userForm = new UserForm(document.getElementById('root'), user);
 userForm.render();
--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -1,6 +1,9 @@
+import { User } from '../models/User';
+
 export class UserForm {
     constructor(
-        public parent: Element
+        public parent: Element,
+        public model: User
     ) {}
 
     eventsMap(): { [key: string]: () => void } {
@@ -17,6 +20,8 @@ export class UserForm {
         return `
         <div>
         <h1>User Form</h1>
+        <p>User name: ${this.model.get('name')}</p>
+        <p>User age: ${this.model.get('age')}</p>
         <input />
         <button>Click</button>
         </div>
